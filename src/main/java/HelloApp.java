7e33276ff85796e6eec5d58aa3581ead4808eef8
Shiva <siva6247769@gmail.com>
2026-03-11 14:25:28 +0530

    UC4: Display greeting for multiple command-line names

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,7 +2,19 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length == 0) {
+
+            System.out.println("Hello World");
+
+        } else {
+
+            for (int i = 0; i < args.length; i++) {
+
+                System.out.println("Hello " + args[i]);
+
+            }
+
+        }
 
     }
 
