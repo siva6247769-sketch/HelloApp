486f584fe7c101bcb99c80abba4cf039503bc55f
Shiva <siva6247769@gmail.com>
2026-03-11 14:21:13 +0530

    UC3: Display greeting with default name when no argument provided

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,8 +2,14 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        String name;
 
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello " + name);
     }
-
 }
