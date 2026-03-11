770930ea5c45e49cb8b8153b4491e31fffaf551c
Shiva <siva6247769@gmail.com>
2026-03-11 14:18:14 +0530

    UC2: Display greeting using command-line argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,8 +2,9 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        String name = args[0];
+
+        System.out.println("Hello " + name);
 
     }
-
 }
