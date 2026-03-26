78eb1c018baff067a83dfd1ec76d093c900106d0
Shiva <siva6247769@gmail.com>
2026-03-26 14:56:19 +0530

    UC6: Display Hello using enhanced for loop and substring method

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,12 +1,23 @@
 public class HelloApp {
     public static void main(String[] args) {
 
+        // Default case
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
+
+            String result = "";
+
+            // Enhanced for loop
             for (String name : args) {
-                System.out.println("Hello, " + name + "!");
+                result += name + ", ";
             }
+
+            // Remove last ", " using substring
+            result = result.substring(0, result.length() - 2);
+
+            // Final output
+            System.out.println("Hello, " + result + "!");
         }
     }
 }
